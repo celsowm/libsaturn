2956b6096d9783540328c9f3896e843796239062
celsowm <celsowm@gmail.com>
2026-09-17 14:00:10 -0300

    tests: make VDP1 texture gate portable

--- tests/host/test_vdp1_native_texture_type.cpp
+++ tests/host/test_vdp1_native_texture_type.cpp
@@ -10,7 +10,7 @@ static_assert(std::is_same_v<decltype(sat_sprite_cmd_t::texture), const sat_vdp1
 static_assert(std::is_same_v<decltype(sat_scaled_sprite_cmd_t::texture), const sat_vdp1_texture_t*>);
 static_assert(std::is_same_v<decltype(sat_distorted_sprite_cmd_t::texture), const sat_vdp1_texture_t*>);
 static_assert(std::is_same_v<decltype(sat_mesh_draw_t::textures), const sat_vdp1_texture_t*>);
-static_assert(std::is_same_v<decltype(sat_ascii_font_t::glyphs[0]), sat_vdp1_texture_t>);
+static_assert(std::is_same_v<std::remove_extent_t<decltype(sat_ascii_font_t::glyphs)>, sat_vdp1_texture_t>);
 
 int main() {
     sat_vdp1_texture_t texture{};
