5e10ae669768a5dfe2cc66557eef70007f58ec25
celsowm <celsowm@gmail.com>
2026-09-18 23:43:22 -0300

    save: export save API from umbrella header

--- include/saturn/saturn.h
+++ include/saturn/saturn.h
@@ -8,6 +8,7 @@
 #include "saturn/geometry2d.h"
 #include "saturn/color.h"
 #include "saturn/memory.h"
+#include "saturn/save.h"
 #include "saturn/time.h"
 #include "saturn/surface.h"
 #include "saturn/texture.h"
